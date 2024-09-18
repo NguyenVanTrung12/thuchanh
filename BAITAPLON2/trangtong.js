fafb1ed97ac8e9984d1ec01d80e7f366c775b290
Nhunghoccode <nhung1379nvc@gmail.com>
2024-09-18 23:04:23 +0700

    cac trang

---
+++ BAITAPLON2/trangtong.js
@@ -0,0 +1,12 @@
+import Footer from './footer';
+import Header from './header';
+import Home from './home';
+export default function Trangtong() {
+    return (
+        <div>
+       <Header></Header>
+       <Home></Home>
+       <Footer></Footer>
+        </div>
+    );
+}
